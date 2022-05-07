5c10b2de798c76f7820acc9ccff7a3b4273230ef
numallo <nuriamafe256@gmail.com>
2022-05-07 10:35:57 +0200

    First Commit

---
+++ src/main/java/com/ironhack/WondercodersCRMDataLayer/WonderCodersCrmDataLayerApplication.java
@@ -0,0 +1,13 @@
+package com.ironhack.WondercodersCRMDataLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderCodersCrmDataLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WonderCodersCrmDataLayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ironhack/WondercodersCRMDataLayer/WonderCodersCrmDataLayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ironhack.WondercodersCRMDataLayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WonderCodersCrmDataLayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
