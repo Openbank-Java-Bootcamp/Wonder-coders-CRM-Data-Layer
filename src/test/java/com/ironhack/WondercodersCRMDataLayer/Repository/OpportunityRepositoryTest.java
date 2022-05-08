8f4da9294ec31a08566eec97c0f47155f517ba1a
numallo <nuriamafe256@gmail.com>
2022-05-08 19:29:39 +0200

    Opportunity update

--- src/test/java/com/ironhack/WondercodersCRMDataLayer/Repository/OpportunityRepositoryTest.java
+++ src/test/java/com/ironhack/WondercodersCRMDataLayer/Repository/OpportunityRepositoryTest.java
@@ -1,10 +1,8 @@
 package com.ironhack.WondercodersCRMDataLayer.Repository;
 
-import com.ironhack.WondercodersCRMDataLayer.Enums.Product;
-import com.ironhack.WondercodersCRMDataLayer.Enums.Status;
-import com.ironhack.WondercodersCRMDataLayer.Model.Opportunity;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import com.ironhack.WondercodersCRMDataLayer.Enums.*;
+import com.ironhack.WondercodersCRMDataLayer.Model.*;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -16,11 +14,30 @@ import static org.junit.jupiter.api.Assertions.*;
 class OpportunityRepositoryTest {
     @Autowired
     private OpportunityRepository opportunityRepository;
+    @Autowired
+    private ContactRepository contactRepository;
+    @Autowired
+    private AccountRepository accountRepository;
+    @Autowired
+    private SalesRepRepository salesRepRepository;
 
     private List<Opportunity> opportunities;
+    private List<Contact> contacts;
+    private List<Account> accounts;
+    private List<SalesRep> salesReps;
 
     @BeforeEach
     void setUp() {
+        contacts = contactRepository.saveAll(List.of(
+                new Contact("Nuria", "123456789", "nuria@gmail.com"),
+                new Contact("Bea", "987654321", "bea@gmail.com"),
+                new Contact("Olatz", "112233445", "olatz@gmail.com"),
+                new Contact("Paula", "998877665", "olatz@gmail.com")
+        ));
+
+        /*opportunities = opportunityRepository.saveAll(List.of(
+                new Opportunity(Product.FLATBED, 40, Status.OPEN, "Nuria", )
+        ));*/
     }
 
     @AfterEach
