71a64f68b9fcf330d3293880af03ee1a02efb0f1
numallo <nuriamafe256@gmail.com>
2022-05-08 21:24:01 +0200

    Update opportunity repository

--- src/test/java/com/ironhack/WondercodersCRMDataLayer/Repository/OpportunityRepositoryTest.java
+++ src/test/java/com/ironhack/WondercodersCRMDataLayer/Repository/OpportunityRepositoryTest.java
@@ -36,15 +36,18 @@ class OpportunityRepositoryTest {
         ));
 
         accounts = accountRepository.saveAll(List.of(
-                new Account("Mercadona", Industry.MEDICAL, 30, "Sueca", "Spain")
+                new Account("Mercadona", Industry.MEDICAL, 30, "Sueca", "Spain"),
+                new Account("Consum", Industry.PRODUCE, 40, "Cullera", "Spain")
         ));
 
         salesReps = salesRepRepository.saveAll(List.of(
-                new SalesRep("Rep")
+                new SalesRep("Rep"),
+                new SalesRep("Rep2")
         ));
 
         opportunities = opportunityRepository.saveAll(List.of(
-                new Opportunity(Product.FLATBED, 40, Status.OPEN, contacts.get(0), accounts.get(0), salesReps.get(0))
+                new Opportunity(Product.FLATBED, 40, Status.OPEN, contacts.get(0), accounts.get(0), salesReps.get(0)),
+                new Opportunity(Product.BOX, 20, Status.CLOSED_LOST, contacts.get(1), accounts.get(1), salesReps.get(1))
         ));
     }
 
@@ -56,9 +59,9 @@ class OpportunityRepositoryTest {
         contactRepository.deleteAll();
     }
 
-@Test
+    @Test
     public void findAllBySalesRepId_ValidData_ListOpportunities() {
-    List<Object[]> result = opportunityRepository.findAllBySalesRepId();
-    assertEquals(1, result.size());
-}
+        List<Object[]> result = opportunityRepository.findAllBySalesRepId();
+        assertEquals(2, result.size());
+    }
 }
