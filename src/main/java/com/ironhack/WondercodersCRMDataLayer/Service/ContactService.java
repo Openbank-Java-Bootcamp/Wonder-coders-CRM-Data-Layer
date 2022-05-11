093026966a5f3e0d86edd8866772e6dfdaa16e8c
oizagirre001 <101482807+oizagirre001@users.noreply.github.com>
2022-05-11 20:17:45 +0200

    Tables fixed (#20)

---
+++ src/main/java/com/ironhack/WondercodersCRMDataLayer/Service/ContactService.java
@@ -0,0 +1,54 @@
+package com.ironhack.WondercodersCRMDataLayer.Service;
+
+import com.ironhack.WondercodersCRMDataLayer.Model.Contact;
+import com.ironhack.WondercodersCRMDataLayer.Repository.ContactRepository;
+import com.ironhack.WondercodersCRMDataLayer.classes.App;
+import com.ironhack.WondercodersCRMDataLayer.classes.AppHelp;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ContactService {
+
+    @Autowired
+    private ContactRepository contactRepository;
+
+    App app = new App();
+
+    AppHelp appHelp = new AppHelp();
+
+    public void showContacts() {
+        List<Contact> contacts = contactRepository.findAll();
+        String title = "CONTACT LIST";
+        String[] headers = {"ID      ", "NAME                              ", "PHONE NUMBER                      ", "EMAIL                              "};
+        List<String[]> list = new ArrayList<>();
+        for (Contact contact : contacts) {
+            String[] values = {Integer.toString(contact.getContactId()), contact.getName(), contact.getPhoneNumber(), contact.getEmail()};
+            list.add(values);
+        }
+        appHelp.printTable(title, headers, list);
+    }
+
+    public void showContact(Contact newContact) {
+        String title = "CONTACT";
+        String[] headers = {"ID      ", "NAME                              ", "PHONE NUMBER                      ", "EMAIL                              "};
+        List<String[]> list = new ArrayList<>();
+        String[] values = {Integer.toString(newContact.getContactId()), newContact.getName(), newContact.getPhoneNumber(), newContact.getEmail()};
+        list.add(values);
+        appHelp.printTable(title, headers, list);
+    }
+
+    public void lookUpContact() {
+        int id = Integer.parseInt(app.getCurrentId());
+        if (contactRepository.findById(id).isPresent()) {
+            showContact(contactRepository.findById(id).get());
+        } else {
+            System.err.println("No contact matches '" + id + "' --> Type 'show contacts' to see the list of available ids.");
+        }
+    }
+
+
+}
